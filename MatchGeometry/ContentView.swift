9a37ad3bd2f65e050646dcdc2e4d7912c3f899e1
Shubham Kamdi <shubhamvkamdi@gmail.com>
2022-11-19 16:48:46 -0800

    Initial Commit

---
+++ MatchGeometry/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MatchGeometry
+//
+//  Created by Shubham Kamdi on 11/19/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MatchGeometry/MatchGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchGeometryApp.swift
+//  MatchGeometry
+//
+//  Created by Shubham Kamdi on 11/19/22.
+//
+
+import SwiftUI
+
+@main
+struct MatchGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
