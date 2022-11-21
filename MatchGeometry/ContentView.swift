2503b08092e6d37792b46c5dca99b0b125621eb1
Shubham Kamdi <shubhamvkamdi@gmail.com>
2022-11-20 17:23:41 -0800

    - Initial

--- MatchGeometry/ContentView.swift
+++ MatchGeometry/ContentView.swift
@@ -8,10 +8,86 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var switchedView: Bool = false
+    @State private var nameSpaceId1: String = ""
+    @State private var nameSpaceId2: String = "id1"
+    @State private var nameSpaceId3: String = ""
+    @State private var nameSpaceId4: String = ""
+    @Namespace private var flipNamespace
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        slidingView()
     }
+    
+    func slidingView() -> some View {
+        HStack {
+            
+            disappearingComponent(
+                nameSpaceId1,
+                "Text 1", {
+                    flipNamespaceHandler("id1", "", "", "")
+                })
+            disappearingComponent(
+                nameSpaceId2,
+                "Text 2", {
+                    flipNamespaceHandler("", "id1", "", "")
+                })
+            disappearingComponent(
+                nameSpaceId3,
+                "Text 3", {
+                    flipNamespaceHandler("", "", "id1", "")
+                })
+            disappearingComponent(
+                nameSpaceId4,
+                "Text 4", {
+                    flipNamespaceHandler("", "", "", "id1")
+                })
+            
+        }
+        
+        .frame(maxWidth: .infinity)
+        .frame( height: 45)
+    }
+    
+    func disappearingComponent(
+        _ namespace: String,
+        _ text: String,
+        _ gestureHandler: @escaping () -> ()
+    ) -> some View {
+        ZStack {
+            if namespace != "" {
+                Rectangle()
+                    .fill(.white)
+                    .shadow(radius: 5)
+                    
+                    .matchedGeometryEffect(
+                        id: namespace,
+                        in: flipNamespace
+                    )
+            }
+            Text(text)
+                .foregroundColor(namespace != "" ? .black : .gray)
+                .frame(maxWidth: .infinity)
+                .onTapGesture {
+                    withAnimation(.spring()) {
+                        gestureHandler()
+                    }
+                }
+        }
+    }
+    
+    func flipNamespaceHandler(
+        _ nameSpace1: String,
+        _ nameSpace2: String,
+        _ nameSpace3: String,
+        _ nameSpace4: String) {
+        self.nameSpaceId1 = nameSpace1
+        self.nameSpaceId2 = nameSpace2
+        self.nameSpaceId3 = nameSpace3
+        self.nameSpaceId4 = nameSpace4
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
